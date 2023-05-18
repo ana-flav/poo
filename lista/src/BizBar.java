49a9bcf02242a046a4a7cf8f02fdfd9861921a99
Ana Flavia <128156301+ana-flav@users.noreply.github.com>
2023-05-18 17:26:10 -0300

    Add files via upload

---
+++ lista/src/BizBar.java
@@ -0,0 +1,11 @@
+public class BizBar{
+    public static void main(String []args){
+    int i = 0;
+    while(i<10){
+    for(int j=0; j<5; j++)
+    System.out.print(j);
+    j++;
+    i++;
+    }
+    }
+    }
